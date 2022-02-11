99917f5b5ae6be5e417149ecef0dcbb564ba12b4
Alexandre Moreira <4lexandre.moreira@gmail.com>
2022-02-11 15:26:07 -0300

    Created base app

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./app.css";
+
+function App() {
+	const [questionNumber, setQuestionNumber] = useState(1);
+	const moneyPyramid = [
+		{ id: 1, amount: "$ 100" },
+		{ id: 2, amount: "$ 200" },
+		{ id: 3, amount: "$ 300" },
+		{ id: 4, amount: "$ 500" },
+		{ id: 5, amount: "$ 1.000" },
+		{ id: 6, amount: "$ 2.000" },
+		{ id: 7, amount: "$ 4.000" },
+		{ id: 8, amount: "$ 8.000" },
+		{ id: 9, amount: "$ 16.000" },
+		{ id: 10, amount: "$ 32.000" },
+		{ id: 11, amount: "$ 64.000" },
+		{ id: 12, amount: "$ 125.000" },
+		{ id: 13, amount: "$ 250.000" },
+		{ id: 14, amount: "$ 500.000" },
+		{ id: 15, amount: "$ 1.000.000" },
+
+	].reverse();
+
+	return (
+		<div className="app">
+			<div className="main">
+				<div className="top">
+					<div className="timer">30</div>
+				</div>
+				<div className="bottom">questions and answers</div>
+			</div>
+			<div className="pyramid">
+				<ul className="moneyList">
+					{moneyPyramid.map((m) => 
+						<li className={questionNumber === m.id ? "moneyListItem active" : "moneyListItem"}>
+							<span className="moneyListItemNumber">{m.id}</span>
+							<span className="moneyListItemAmount">{m.amount}</span>
+						</li>
+					)}
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default App;
